8ff501fcdc096e319c46ace8e4959c1a2960168b
Felix Hillebrand <97hilfel@gmail.com>
2023-04-06 09:32:26 +0200

    Setup build and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Environment } from './environment';
 import log4js from "log4js";
 
 log4js.configure({
@@ -6,10 +5,9 @@ log4js.configure({
   categories: { default: { appenders: ["console"], level: "info" } },
 });
 
-const env = Environment.getInstance();
 const log = log4js.getLogger();
 
-log.info(`Sentry Token set: ${env.SENTRY_TOKEN != null}`);
+log.info(`Sentry Token set: `);
 
 process.on("SIGINT", () => {
   // Graceful shutdown here
